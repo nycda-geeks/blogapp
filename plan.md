9db46eaa8edbbe5164051d417bc55482530f4e9f
Sarith Breedijk <sarith@live.nl>
2016-06-03 16:55:37 +0200

    "added css and made it a lot smoother working"

---
+++ plan.md
@@ -0,0 +1,43 @@
+Blog App
+
+Summary
+
+Project Specification
+Create a blogging application that allows users to do the following:
+- register an account
+- login
+- logout
+
+Once logged in, a user should be able to:
+- create a post
+- view a list of their own posts
+- view a list of everyone's posts
+- view a specific post, including the comments people have made about it
+- leave a comment on a post
+
+Prior to coding, determine the following:
+- your tables: what columns will they have? How will they connect to one another?
+- make a diagram showing the relationships between tables.
+- your routes: what routes should you have? What should each route do?
+Once you are done designing your application, then proceed with coding.
+Submit this document in a text file as part of your application.
+
+Other requirements:
+Your routes must be "RESTful". See slide 4 of the http requests lecture: Link. Also look at the RESTful routing example in the node sample apps: Link (Links to an external site.)
+You must use Sequelize for this assignment. Your connection string must once again be read from the environment variables you set up for the Bulletin Board assignment.
+Commit well - each commit should ideally be a new piece of functionality.
+
+
+1. mappen maken
+2. create table
+3. create routes
+	1. Log in
+	2. Register
+	3. Create post
+	4. view posts
+		1. own
+		2. all
+		3. specific
+		4. comment
+
+
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+
 var sequelize = new Sequelize('sarithbreedijk', 'sarithbreedijk', null, {
 	host: 'localhost',
 	dialect: 'postgres',
@@ -11,15 +12,40 @@ var sequelize = new Sequelize('sarithbreedijk', 'sarithbreedijk', null, {
 	}
 });
 
-var User = sequelize.define('user', {
+
+var user = sequelize.define('user', {
 	name: Sequelize.STRING,
 	email: Sequelize.STRING,
 	password: Sequelize.STRING,
-	primaryKey: true
-
 });
 
+
+var message = sequelize.define('messages', {
+	title: Sequelize.STRING,
+	body: Sequelize.TEXT,
+	user_id: Sequelize.INTEGER
+});
+
+var comment = sequelize.define('comment', {
+	title: Sequelize.STRING,
+	body: Sequelize.TEXT,
+	user_id: Sequelize.INTEGER
+});
+
+user.hasMany(message)
+user.hasMany(comment)
+comment.belongsTo(user)
+comment.belongsTo(message)
+message.hasMany(comment)
+message.belongsTo(user)
+
+sequelize.sync().then(function () {
+	console.log('sync done')
+});
+
+
 var app = express();
+
 
 app.use(session({
 	secret: 'oh wow very secret much security',
@@ -27,28 +53,27 @@ app.use(session({
 	saveUninitialized: false
 }));
 
+
 app.set('views', './src/views');
 app.set('view engine', 'jade');
+app.use(express.static('src'));
+app.use(express.static('../js'));
 
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
 	response.render('index', {
-		// message: request.query.message,
-		// user: request.session.user
+		message: request.query.message,
+		user: request.session.user
 	});
 });
 
-// app.get('/profile', function (request, response) {
-// 	var user = request.session.user;
-// 	if (user === undefined) {
-// 		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
-// 	} else {
-// 		response.render('profile', {
-// 			user: user
-// 		});
-// 	}
-// });
 
-app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
+app.get('/login', function(request, response) {
+	response.render('login', {
+	});
+});
+
+
+app.post('/login', bodyParser.urlencoded({extended: true}), function(request, response) {
 	if(request.body.email.length === 0) {
 		response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
 		return;
@@ -59,14 +84,14 @@ app.post('/login', bodyParser.urlencoded({extended: true}), function (request, r
 		return;
 	}
 
-	User.findOne({
+	user.findOne({
 		where: {
 			email: request.body.email
 		}
-	}).then(function (user) {
+	}).then(function(user) {
 		if (user !== null && request.body.password === user.password) {
 			request.session.user = user;
-			response.redirect('/profile');
+			response.redirect('/');
 		} else {
 			response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
 		}
@@ -75,7 +100,39 @@ app.post('/login', bodyParser.urlencoded({extended: true}), function (request, r
 	});
 });
 
-app.get('/logout', function (request, response) {
+
+app.get('/profile', function(request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+	} else {
+	var ID = request.session.user.id;
+	message.findAll({
+		where:{
+			user_id: ID,
+		}
+	}).then(function (messages) {
+	var Data = messages.map(function (appmessage) {
+		return {
+			title: appmessage.dataValues.title,
+			body: appmessage.dataValues.body,
+			user_id: appmessage.dataValues.user_id
+		}
+	
+	})	
+	var allOwnMessages = Data;
+		// console.log(allOwnMessages);
+		console.log(allOwnMessages);
+		response.render('profile', {
+			allOwnMessages: allOwnMessages
+				});
+	})		
+};
+});
+
+
+
+app.get('/logout', function(request, response) {
 	request.session.destroy(function(error) {
 		if(error) {
 			throw error;
@@ -84,23 +141,102 @@ app.get('/logout', function (request, response) {
 	})
 });
 
-// sequelize.sync({force: true}).then(function () {
-// 	User.create({
-// 		name: "sarith",
-// 		email: "sarith@live.nl",
-// 		password: "hahahazeg ik niet"
-// 	}).then(function () {
-// 		var server = app.listen(3000, function () {
-// 			console.log('Example app listening on port: ' + server.address().port);
-// 		});
-// 	});
-// }, function (error) {
-// 	console.log('sync failed: ');
-// 	console.log(error);
-// });
 
+app.get('/register', function(request, response) {
+	response.render('register', {
+	});
+});
+
+
+app.post('/register', bodyParser.urlencoded({extended: true}), function(request, response) {
+	user.create({
+		name: request.body.name,
+		email: request.body.email,
+		password: request.body.password
+	}).then(function(){
+		if (typeof(user) == 'undefined'){
+  			response.redirect('/login')
+  		} if (typeof(user) !== 'undefined') {
+  			response.redirect('/?message=' + encodeURIComponent("This user already exists"));
+		return;
+  		}
+
+	});
+});
+
+
+
+app.get('/createpost', function(request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+	} else {
+	response.render('createpost', {
+	});
+};
+});
+
+
+app.post('/createpost', bodyParser.urlencoded({extended: true}), function(request, response) {
+	var ID = request.session.user.id;
+
+	message.create({
+		title: request.body.title,
+		body: request.body.body,
+		user_id: ID
+
+		}).then(function(){
+  			response.redirect('/overview')
+  		});
+  	});
+  	
+
+app.get('/overview', function(request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+	} else {
+	message.findAll().then(function (messages) {
+	var data = messages.map(function (message) {
+		return {
+			title: message.dataValues.title,
+			body: message.dataValues.body
+		};	
+	});
+	var allMessages = data;
+	response.render('overview', {
+			allMessages:allMessages
+		});
+	}); 
+	};
+});
+
+app.get('/specific', function(request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+	} else {
+	user.findAll().then(function (users) {
+	var everyOne = users.map(function (user) {
+		return {
+			name: appmessage.dataValues.name
+		}
+	});
+	var allUsers = data;
+		// console.log(allOwnMessages);
+		console.log(allOwnMessages);
+		response.render('specific', {
+			allUsers: allUsers
+				});
+	})		
+};
+});
+
+
+
+ 
 var server = app.listen(3000, function() {
 	console.log('Example app listening on port: ' + server.address().port);
 });
-;
+
 
